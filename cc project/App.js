6a1534cf25b9972df43bddea1009448847d22dbe
Yogavarshini Gokulavanan <118422217+YgavarshiniGokulavanan@users.noreply.github.com>
2023-12-14 10:17:43 +0530

    Add files via upload

--- cc project/App.js
+++ cc project/App.js
@@ -1,12 +1,64 @@
 // src/App.js
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 
-import Register from './components/Register.jsx';
+const Login = () => {
+  const [Email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    if (Email=== 'varshu@gmail.com' && password === 'varshu') {
+   
+      alert('Login Successful!');
+    } else {
+      setError('Invalid username or password');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit} className="login-form">
+        <h2>WELCOME TO GARDENING</h2>
+        {error && <p className="error-message">{error}</p>}
+        <div className="form-group">
+          <label>Email:</label>
+          <input
+            type="text"
+            value={Email}
+            onChange={handleUsernameChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <button type="submit" className="login-btn">Login</button>
+      </form>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Login />
     </div>
   );
 }
